146f6416adc4ac5c4ccea1325d78d355be28e094
Andy <andyalmeida191@gmail.com>
2022-11-03 11:45:40 -0700

    Init Files

---
+++ designDoc.h
@@ -0,0 +1,19 @@
+//This file is for documentation purposes only. It will be ignored by the compiler.
+//Our Teaching Assistant will manually grade this file.
+
+//Please make sure that you include the entire content of this file as comments.
+
+/* Design Doc: communicate your thought process in English.
+ 
+0. List of members who contributed to the implementation of this project:
+   
+
+ 
+1. Describe your algorithm for the sentenceSplitter( ) function in pseudocode.
+
+
+2. Describe your algorithm for the wordpairMapping( ) function in pseudocode.
+
+ 
+
+*/
